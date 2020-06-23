851041fba164c1302236891270e3837d59c0c79b
Yehiyam Livneh <yehiyam@gmail.com>
2020-06-23 14:28:20 +0300

    add internal config add run factory method

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 module.exports = require('./lib/algorunner');
-module.exports.debug = require('./lib/debugging/debugHelper').debug;
+module.exports.debug = require('./lib/debug');
+module.exports.run = require('./lib/run').run;
+module.exports.config = require('./lib/config');
