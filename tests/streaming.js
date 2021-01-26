7cb1468edc2705921f9ea52a1f82a2713f5758a0
NassiHarel <nassi9@gmail.com>
2021-01-26 17:06:57 +0200

    feat: streaming

--- tests/streaming.js
+++ tests/streaming.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { uuid } = require('@hkube/uid');
 const clone = require('lodash.clonedeep');
 const messages = require('../lib/consts/messages');
-const { waitFor } = require('./utils');
+const { waitFor } = require('../lib/utils/waitFor');
 const delay = d => new Promise(r => setTimeout(r, d));
 const mainConfig = require('../lib/config');
 let Algorunner
@@ -34,8 +34,10 @@ describe('streaming', () => {
         // stateful
         const MAX = 50;
         const statefulCB = {
-            start: (args, hkubeApi) => {
-                Array.from(Array(MAX).keys()).forEach(a => hkubeApi.sendMessage({ data: 'hello stateless' }));
+            start: async (args, hkubeApi) => {
+                Array.from(Array(MAX).keys()).forEach(a => hkubeApi.sendMessage({ data: 'hello yellow' }, 'main'));
+                Array.from(Array(MAX).keys()).forEach(a => hkubeApi.sendMessage({ data: 'hello black' }, 'second'));
+                await delay(5000);
             }
         }
         const stateful = new Algorunner();
@@ -48,23 +50,37 @@ describe('streaming', () => {
             kind: 'stream',
             stateType: 'stateful',
             nodeName: 'green',
-            childs: ['yellow'],
+            childs: ['yellow', 'black'],
             parsedFlow: {
                 main: [{
                     source: 'green',
                     next: ['yellow']
+                },
+                {
+                    source: 'yellow',
+                    next: ['black']
+                }],
+                second: [{
+                    source: 'green',
+                    next: ['black']
                 }]
-            },
-            defaultFlow: 'main'
+            }
         }
         await stateful._init(statefulData);
-        await stateful._start({});
+        stateful._start({});
+        await delay(500);
+
+        const queue = stateful._streamingManager._messageProducer._adapter._messageQueue._everAppended;
+        const { yellow, black } = queue;
+        expect(yellow).to.eql(MAX);
+        expect(black).to.eql(MAX);
 
         // stateless 1
         let countStateless = 0;
         const statelessCB1 = {
             start: (args) => {
                 countStateless += 1;
+                // throw new Error('ooopss');
                 return 42;
             }
         }
@@ -81,6 +97,10 @@ describe('streaming', () => {
             childs: ['black'],
             parsedFlow: {
                 main: [{
+                    source: 'green',
+                    next: ['yellow']
+                },
+                {
                     source: 'yellow',
                     next: ['black']
                 }]
@@ -88,8 +108,7 @@ describe('streaming', () => {
             defaultFlow: 'main'
         }
         await stateless1._init(statelessData1);
-        stateless1._start({});
-        await stateless1._discoveryUpdate([{ nodeName: 'green', address: { host: 'localhost', port: configStateful.discovery.streaming.port }, type: 'Add' }]);
+        stateless1._discoveryUpdate([{ nodeName: 'green', address: { host: 'localhost', port: configStateful.discovery.streaming.port }, type: 'Add' }]);
 
         // stateless 2
         const statelessCB2 = {
@@ -111,11 +130,24 @@ describe('streaming', () => {
             nodeName: 'black'
         }
         await stateless2._init(statelessData2);
+        stateless2._discoveryUpdate([
+            {
+                nodeName: 'yellow',
+                address: { host: 'localhost', port: configStateless1.discovery.streaming.port },
+                type: 'Add'
+            },
+            {
+                nodeName: 'green',
+                address: { host: 'localhost', port: configStateful.discovery.streaming.port },
+                type: 'Add'
+            }
+        ]);
+        stateless1._start({});
         stateless2._start({});
-        await stateless2._discoveryUpdate([{ nodeName: 'yellow', address: { host: 'localhost', port: configStateless1.discovery.streaming.port }, type: 'Add' }]);
+        await delay(500);
 
-        await waitFor(() => countStateless === MAX * 2);
-        expect(countStateless).to.eql(MAX);
+        await waitFor({ resolveCB: () => countStateless === MAX * 2 });
+        expect(countStateless).to.eql(MAX * 2);
     });
     it('should init with stateful', async () => {
         const callbacks = {
