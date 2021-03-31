46e3d915f105560ad3280e87f4b8004da6268fb2
NassiHarel <nassi9@gmail.com>
2021-03-31 17:37:47 +0300

    feat: logger format

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const Logger = require('@hkube/logger');
+const config = require('./lib/config');
+const log = new Logger(config.serviceName, config.logger); // eslint-disable-line
+
 module.exports = require('./lib/algorunner');
 module.exports.debug = require('./lib/debug');
 module.exports.run = require('./lib/run').run;
-module.exports.config = require('./lib/config');
+module.exports.config = config;
